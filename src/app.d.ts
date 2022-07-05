aa6427418b0d9036dc55c3d2b48030afff90cc93
Steven Ducat <steveducat@gmail.com>
2022-07-05 19:39:22 +0800

    Add hooks.ts examples

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,8 +4,14 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		username: string
+	}
+	interface Session {
+		user?: {
+			name: string
+		}
+	}
 	// interface Platform {}
-	// interface Session {}
 	// interface Stuff {}
 }
